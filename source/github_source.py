06ebe6d16a15399b008ecf204b2dc40de858ae9e
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2025-10-25 20:21:12 +0200

    Add Ghidra TopSM arch plugin.

--- source/github_source.py
+++ source/github_source.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 import io
-import re
 import zipfile
 import requests
 from github import Auth, Github
@@ -23,6 +22,7 @@ class GithubSource(Source):
         "antoniovazquezblanco/GhidraInvalidMemoryRefs",
         "antoniovazquezblanco/GhidraSVD",
         "antoniovazquezblanco/GhidraSystemmap",
+        "antoniovazquezblanco/GhidraTopSM",
         "BartmanAbyss/ghidra-amiga",
         "boricj/ghidra-delinker-extension",
         "chaoticgd/ghidra-emotionengine-reloaded",
