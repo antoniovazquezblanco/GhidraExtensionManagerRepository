4a85d6ef603621499db5206d4b13755c2b819605
Antonio Vázquez <antoniovazquezblanco@gmail.com>
2026-04-17 10:37:51 +0200

    init: Format

--- source/__init__.py
+++ source/__init__.py
@@ -4,8 +4,9 @@
 import importlib
 from pathlib import Path
 
-__all__ = [file.stem for file in Path(__file__).parent.glob(
-    "*.py") if file.stem != "__init__"]
+__all__ = [
+    file.stem for file in Path(__file__).parent.glob("*.py") if file.stem != "__init__"
+]
 
 for m in __all__:
     importlib.import_module(f"{Path(__file__).parent.stem}.{m}")
